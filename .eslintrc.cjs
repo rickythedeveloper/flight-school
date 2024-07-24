c9d591c7a1cca9cae3a96e2c0b5b241cde1faea2
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2024-07-24 20:58:41 +0100

    FSA-2: More eslint rules with typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "error"
+  }
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {ReactNode} from "react";
+import {ReactElement, ReactNode} from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
-}>) {
+}>): ReactElement {
   return (
     <html lang="en">
       <body>{children}</body>
