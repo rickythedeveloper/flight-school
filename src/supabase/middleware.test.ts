aa0e9ed4c622aa5f8923dc96e3c62133750ef2ec
Ricky Kawagishi <52386424+rickythedeveloper@users.noreply.github.com>
2024-08-02 01:31:49 +0100

    Add unit tests for supabase middleware

---
+++ src/supabase/middleware.test.ts
@@ -0,0 +1,67 @@
+import { describe, test, jest, expect, beforeEach } from "@jest/globals";
+import { createServerClient } from "@supabase/ssr";
+import { NextRequest, NextResponse } from "next/server";
+import type { NextURL } from "next/dist/server/web/next-url";
+import { updateSession } from "@/supabase/middleware";
+import { getEnvironment } from "@/utils/environment/environment";
+
+jest.mock("@supabase/ssr");
+const createServerClientMock = createServerClient as jest.Mock;
+
+jest.mock("@/utils/environment/environment");
+const getEnvironmentMock = getEnvironment as jest.Mock;
+
+jest.mock("next/server", () => {
+  return {
+    __esModule: true,
+    ...(jest.requireActual("next/server") as any), // eslint-disable-line @typescript-eslint/no-explicit-any
+    NextResponse: {
+      redirect: jest.fn(),
+      next: jest.fn(),
+    },
+  };
+});
+const redirectMock = NextResponse.redirect as jest.Mock;
+
+describe("supabaseMiddleware", () => {
+  beforeEach(() => {
+    getEnvironmentMock.mockReturnValue({
+      supabase: {
+        url: "supabase-url",
+        anonKey: "supabase-anon-key",
+      },
+    });
+
+    redirectMock.mockReset();
+  });
+
+  test("redirects to login if unauthenticated", async () => {
+    createServerClientMock.mockReturnValue({
+      auth: {
+        getUser: () => Promise.resolve({ data: { user: null } }),
+      },
+    });
+
+    const request = new NextRequest("http://localhost:3000/next-path");
+
+    await updateSession(request);
+
+    const redirectUrl = redirectMock.mock.calls[0][0] as NextURL;
+    expect(redirectUrl.pathname).toBe("/login");
+  });
+
+  test("redirects to next page if authenticated", async () => {
+    createServerClientMock.mockReturnValue({
+      auth: {
+        getUser: () => Promise.resolve({ data: { user: {} } }),
+      },
+    });
+
+    const request = new NextRequest("http://localhost:3000/next-path");
+
+    await updateSession(request);
+
+    expect(NextResponse.redirect).not.toHaveBeenCalled();
+    expect(NextResponse.next).toHaveBeenCalled();
+  });
+});
