64afa4965da4043eb8035bff442c3e143dc07530
Ricky Kawagishi <52386424+rickythedeveloper@users.noreply.github.com>
2024-08-18 15:37:24 +0100

    Add mobiles to playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,29 +33,29 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
+      name: "Desktop Chrome",
       use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: "firefox",
+      name: "Desktop Firefox",
       use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: "webkit",
+      name: "Desktop Safari",
       use: { ...devices["Desktop Safari"] },
     },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 13 Mini"] },
+    },
+
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 7"] },
+    },
 
     /* Test against branded browsers. */
     // {
