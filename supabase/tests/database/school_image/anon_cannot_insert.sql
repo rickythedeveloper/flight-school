392d9868d7974a635cbd37fd5eed98444d6b7154
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2024-09-08 01:57:08 +0100

    Fix db tests

--- supabase/tests/database/school_image/anon_cannot_insert.sql
+++ supabase/tests/database/school_image/anon_cannot_insert.sql
@@ -2,10 +2,9 @@ begin;
 
 select plan(2);
 
-insert into "flight_school"."image" (id, resource_id) values ('5C8735D9-3FD1-4474-9678-67E7DAAF1808', 'BBA87E2C-B802-448D-AC4B-EE87CED45E57');
 insert into "flight_school"."school" (id, name, description) values ('E667F475-BA99-4005-9E17-AE6B8C54827F', 'School A', 'We provide quality teaching.');
 
-prepare insert_image as insert into "flight_school"."school_image" (image_id, school_id) values ('5C8735D9-3FD1-4474-9678-67E7DAAF1808', 'E667F475-BA99-4005-9E17-AE6B8C54827F');
+prepare insert_image as insert into "flight_school"."school_image" (resource_id, school_id) values ('5C8735D9-3FD1-4474-9678-67E7DAAF1808', 'E667F475-BA99-4005-9E17-AE6B8C54827F');
 
 set role anon;
 select throws_ok('insert_image', '42501', 'new row violates row-level security policy for table "school_image"', 'anon user should not be able to insert a school image');
--- supabase/tests/database/school_image/cannot_insert_without_resource_id.sql
+++ supabase/tests/database/school_image/cannot_insert_without_resource_id.sql
@@ -7,6 +7,6 @@ insert into "flight_school"."school" (id, name, description) values ('91E6A70F-D
 prepare insert_image as insert into "flight_school"."school_image" (school_id) values ('91E6A70F-DA73-4728-82A7-479D08CC1841');
 
 set role authenticated;
-select throws_ok('insert_image', '23502', 'null value in column "image_id" of relation "school_image" violates not-null constraint', 'users should not be able to insert a school image without linking to an image');
+select throws_ok('insert_image', '23502', 'null value in column "resource_id" of relation "school_image" violates not-null constraint', 'users should not be able to insert a school image without a resource id');
 
 rollback;
